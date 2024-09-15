ef4ee2636dcce4afc8ac5b770db83aae5c568cd8
divya <dchorage567@gmail.com>
2024-09-15 10:11:48 +0530

    angularPhonePay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'phonepay';
+  
+  WhatToShow:number=1;
+  id:number=0;
+  baseUrl='http://localhost:9090/'
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import {FormsModule} from  '@angular/forms';
+
+import {HttpClientModule} from  '@angular/common/http';
+import { LinkAccountComponent } from './dashboard/link-account/link-account.component';
+
+import { SendMoneyComponent } from './dashboard/send-money/send-money.component';
+import { HistoryComponent } from './dashboard/history/history.component';
+import { ProfileComponent } from './dashboard/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    LinkAccountComponent,
+    SendMoneyComponent,
+    HistoryComponent,
+    ProfileComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
